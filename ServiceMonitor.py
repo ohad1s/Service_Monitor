5492f76e13f99d5e75c60ed058bef9ce01f75dbf
Ohad Shirazi <92723105+ohad1s@users.noreply.github.com>
2022-03-26 22:40:03 +0300

    almost finished

--- ServiceMonitor.py
+++ ServiceMonitor.py
@@ -1,7 +1,7 @@
 import os
 import platform
+import re
 import sys
-import threading
 from _datetime import datetime
 import time
 import subprocess
@@ -26,41 +26,41 @@ def runWindows():
 def runUbuntu():
     os.system("systemctl list-units --type=service |awk '{print $1,$4}' | sed -E 's/ +/,/g' > service.csv")
 
+
 ############################# sample ########################################
 def sample(log_file):
     dict = {}
-    date = datetime.datetime.now()
-    log_file.write("{}\n".format(date))
     o_s()
     open_ser_csv = open("service.csv", encoding="utf8")
     read_ser_csv = reader(open_ser_csv)
     services_data = list(read_ser_csv)
-    log_file.write("---------------------"+str(datetime.now())+"--------------------------\n")
+    date_ = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+    log_file.write("---------------------" + date_ + "--------------------------\n")
     for i in range(2, len(services_data) - 1):
         service_name = services_data[i][8]
         service_status = services_data[i][11]
         line_to_write = "{} {}\n".format(service_name, service_status)
         log_file.write(line_to_write)
-        log_file.write("\n")
-        dict[service_name]=service_status
+        # log_file.write("\n")
+        dict[service_name] = service_status
     log_file.close()
     return dict
 
+
 ############################# sample_diff ########################################
 
-def sample_diff(log_file,sample1,sample2):
+def sample_diff(log_file, sample1, sample2):
     for service_, status_ in sample1.items():
-        date = datetime.datetime.now()
+        date = datetime.now()
         if service_ not in sample2:
-            str = "Service {} is found at sample 1 but not sample 2. This service probably was uninstalled".format(service_)
+            str = "Service {} is found at sample 1 but not sample 2. This service probably was uninstalled".format(
+                service_)
             print(str)
             log_file.write(str + "\n")
-            # log_file.flush()
         elif status_ != sample2[service_]:
             str = "{}: Service '{}' changed status from '{}' to '{}'".format(date, service_, status_, sample2[service_])
             print(str)
             log_file.write(str + "\n")
-            # log_file.flush()
 
 
 # ############################ monitor ###########################################
@@ -142,10 +142,91 @@ def sample_diff(log_file,sample1,sample2):
 
 ########################### o_s ########################################################
 def o_s():
-    if platform.system() == "Windows":
+    if platform == "Windows":
         runWindows()
     else:
         runUbuntu()
+
+
+########################### filter with 2 dates ######################################################
+def filterByDates(date1, date2):
+    date1_log = {}
+    date2_log = {}
+    log_file = open(SERVICE_LIST_FILE, "r")
+    read_log_file = reader(log_file)
+    log_lines = list(read_log_file)
+    # print(log_lines)
+
+    for line in log_lines:
+        if line[0].startswith("---"):
+
+            date_0 = re.search(r"[\d]{1,2}/[\d]{1,2}/[\d]{4}", line[0]).span()[0]
+            date_1 = re.search(r"[\d]{1,2}/[\d]{1,2}/[\d]{4}", line[0]).span()[1]
+            date_from_log = line[0][date_0:date_1]
+
+            hour_0 = re.search(r"[\d]{1,2}:[\d]{1,2}:[\d]{1,2}", line[0]).span()[0]
+            hour_1 = re.search(r"[\d]{1,2}:[\d]{1,2}:[\d]{1,2}", line[0]).span()[1]
+            hour_from_logs = line[0][hour_0:hour_1]
+
+            # hour_from_logs = re.search(r"[\d]{1,2}:[\d]{1,2}:[\d]{1,2}",line[0])
+            # match = re.search(r"\s(\d{2}\:\d{2}\",line)
+
+            date_0_to_check = re.search(r"[\d]{1,2}/[\d]{1,2}/[\d]{4}", date1).span()[0]
+            date_1_to_check = re.search(r"[\d]{1,2}/[\d]{1,2}/[\d]{4}", date1).span()[1]
+            date_to_check = date1[date_0_to_check:date_1_to_check]
+
+
+            hour_0_to_check = re.search(r"[\d]{1,2}:[\d]{1,2}:[\d]{1,2}", date1).span()[0]
+            hour_1_to_check = re.search(r"[\d]{1,2}:[\d]{1,2}:[\d]{1,2}", date1).span()[1]
+            hour_to_check = date1[hour_0_to_check:hour_1_to_check]
+
+            print(date_from_log)
+            print(date_to_check)
+            print(hour_from_logs)
+            print(hour_to_check)
+            if date_from_log == date_to_check and hour_from_logs == hour_to_check:
+                print("yes")
+                index1 = log_lines.index(line)
+                print(index1)
+                for i in range(index1 + 1, len(log_lines)):
+                    if not log_lines[i][0].startswith("---"):
+                        service_and_status = log_lines[i][0].split(" ")
+                        date1_log[service_and_status[0]] = service_and_status[1]
+                    else:
+                        break
+
+            date_0_to_check2 = re.search(r"[\d]{1,2}/[\d]{1,2}/[\d]{4}", date2).span()[0]
+            date_1_to_check2 = re.search(r"[\d]{1,2}/[\d]{1,2}/[\d]{4}", date2).span()[1]
+            date_to_check2 = date2[date_0_to_check2:date_1_to_check2]
+
+
+            hour_0_to_check2 = re.search(r"[\d]{1,2}:[\d]{1,2}:[\d]{1,2}", date2).span()[0]
+            hour_1_to_check2 = re.search(r"[\d]{1,2}:[\d]{1,2}:[\d]{1,2}", date2).span()[1]
+            hour_to_check2 = date2[hour_0_to_check2:hour_1_to_check2]
+
+            if date_from_log == date_to_check2 and hour_from_logs == hour_to_check2:
+                index2 = log_lines.index(line)
+                print(index2)
+                for i in range(index2 + 1, len(log_lines)):
+                    if not log_lines[i][0].startswith("---"):
+                        service_and_status = log_lines[i][0].split(" ")
+                        date2_log[service_and_status[0]] = service_and_status[1]
+                    else:
+                        break
+    sample_diff(open(STATUS_LOG_FILE, "a"), date1_log, date2_log)
+
+    #
+
+
+# for line in log_file:
+# 	str_line_date = line[0:19]
+# 	line_date = validDate(str_line_date)
+# 	if line_date == False:
+# 		print("> Something went wront with date conversion of status log")
+# 		exit()
+# 	if date1 <= line_date <= date2:
+# 		result.append(line)
+# return result
 
 ############################### main #########################################
 if __name__ == '__main__':
@@ -156,7 +237,7 @@ if __name__ == '__main__':
     # Monitor mode
     if ("monitor" == sys.argv[1]):
         if (len(sys.argv) <= 2):
-            raise "Time not selected Error!\n please enter how much seconds to refresh monitor"
+            raise "Time not selected Error! please enter how much seconds to refresh monitor"
             # print("Enter how much seconds to refresh monitor")
             # exit()
         # Get seconds
@@ -164,57 +245,43 @@ if __name__ == '__main__':
         str = "> Monitor mode: Refresh rate of {} seconds".format(seconds)
         print(str)
 
-        initFiles()
         platform = platform.system()
-        status_log = open(STATUS_LOG_FILE, "a")
-        log_file = open(SERVICE_LIST_FILE, "a")
-        ###################################### Windows Platform ######################################
-        if (platform == "Windows"):
-            print("> Windows detected")
-            dict = Win_SampleToLog(log_file)
-            while True:
-                my_dict = Win_SampleToLog(open(SERVICE_LIST_FILE, "a"))
-                time.sleep(float(seconds))
-                my_dict2 = Win_SampleToLog(open(SERVICE_LIST_FILE, "a"))
-                DiffSamples(status_log, my_dict, my_dict2, platform)
+        # status_log = open(STATUS_LOG_FILE, "a")
+        # log_file = open(SERVICE_LIST_FILE, "a")
+        #
+        #
+        # dict = sample(log_file)
+        while True:
+            my_dict = sample(open(SERVICE_LIST_FILE, "a"))
+            time.sleep(float(seconds))
+            my_dict2 = sample(open(SERVICE_LIST_FILE, "a"))
+            sample_diff(open(STATUS_LOG_FILE, "a"), my_dict, my_dict2)
 
-
-
-        ###################################### Linux Platform ######################################
-        else:
-            print("> Linux detected")
-            dict = Linux_SampleToLog(log_file)
-            while True:
-                my_dict = Linux_SampleToLog(open(SERVICE_LIST_FILE, "a"))
-                time.sleep(float(seconds))
-                my_dict2 = Linux_SampleToLog(open(SERVICE_LIST_FILE, "a"))
-                DiffSamples(status_log, my_dict, my_dict2, platform)
 
     elif ("manual" == sys.argv[1]):
         print("> Manual mode")
         if (len(sys.argv) <= 5):
-            print("Please enter 2 dates for sample range")
-            exit()
+            raise "2 dates not selected Error! please enter 2 dates for sample range"
+            # print("Please enter 2 dates for sample range")
+            # exit()
         txt_date1 = sys.argv[2] + " " + sys.argv[3]
         txt_date2 = sys.argv[4] + " " + sys.argv[5]
-
-        date1 = validDate(txt_date1)
-        date2 = validDate(txt_date2)
-        if date1 == False or date2 == False:
-            print("Please try again")
-            exit()
-
+        # mipo:
+        # date1 = validDate(txt_date1)
+        # date2 = validDate(txt_date2)
+        # if date1 == False or date2 == False:
+        #      raise "Please try again"
+        #     # exit()
+        # adpo
         # Success, now search the correct sampleings
-        lines = filterStatusLogByDates(date1, date2)
-        print("> Total events found: " + str(len(lines)))
-        for line in lines:
-            print(line)
-
+        # filterByDates(date1, date2) kolel
+        filterByDates(txt_date1, txt_date2)
+        # print("> Total events found: " + str(len(lines)))
+        # for line in lines:
+        #     print(line)
     else:
-        print("Use 'manual' or 'monitor' mode")
-        exit()
-
-
+        raise "Use 'manual' or 'monitor' mode"
+        # exit()
 
     # time_for_monitor = int(input("How often do you want the monitor to work?  (in SECONDS)"))
     # print("service monitor is working...")
